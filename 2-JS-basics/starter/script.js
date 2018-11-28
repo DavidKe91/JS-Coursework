0e008fdb4af757104e945aacab340d1e00466667
DavidKe91 <davidgkennedy@hotmail.com>
2018-11-28 22:25:54 +0000

    Finished Section 2, Lecture 10

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,51 @@
+/*
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'teacher';
+console.log(job);
+
+
+var firstName = 'John';
+var age = 26;
+
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = "Teacher";
+isMarried = false;
+
+console.log(job  + ' is' + firstName+ ' married?: ' + isMarried);
+
+var lastName = prompt('What is his last name?');
+console.log('His full name is: ' + firstName + lastName);
+*/
+
+var now = 2018;
+ageJohn = 28;
+ageMark = 33;
+var yearJohn = now - 28;
+var yearMark = now - 33;
+
+console.log(yearJohn);
+
+console.log(now - 2);
+console.log(now - 10);
+console.log(now % 2000);
+
+var johnOlder = ageJohn > ageMark;
+console.log(johnOlder);
+
+// typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof now);
